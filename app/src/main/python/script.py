b3184ac0357ff225075c329f5f328fb194b3cb15
GrumpyEric <eric.arszelewski@haw-hamburg.com>
2022-01-16 19:12:50 +0100

    Test, funktioniert alles?

---
+++ app/src/main/python/script.py
@@ -0,0 +1,11 @@
+from math import sqrt
+
+def main(number1, number2):
+    num1 = int(number1)
+    num2 = int(number2)
+
+    sum = num1 + num2
+
+    srt = sqrt(sum)
+
+    return "sum is " +str(srt)
